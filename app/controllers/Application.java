4b8b1004f8a0072cb019a96252d5451797e3265d
Ryan Brushett <ryanbrushett@gmail.com>
2013-12-29 14:36:10 -0500

    Added one line to the javascript routes for deleting crags

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -22,7 +22,8 @@ public class Application extends Controller {
         response().setContentType("text/javascript");
         return ok(
             Routes.javascriptRouter("jsRoutes",
-                controllers.routes.javascript.Crags.addCrag()
+                controllers.routes.javascript.Crags.addCrag(),
+                controllers.routes.javascript.Crags.deleteCrag()
             )
         );
     }
