6d6a538a4dbc28f37dbb881c700538bcc47a065d
Ryan Brushett <ryanbrushett@gmail.com>
2014-01-04 00:55:32 -0330

    Javascript that allows creating and deleting crags is done

--- public/javascripts/myFunctions.js
+++ public/javascripts/myFunctions.js
@@ -11,5 +11,85 @@ function updateactive(){
     }
     if (path == "/crags"){
         document.getElementById("locations").className="active";
+        addCragInit();
     }
+}
+
+Element.prototype.remove = function() {
+    this.parentElement.removeChild(this);
+}
+
+NodeList.prototype.remove = HTMLCollection.prototype.remove = function() {
+    for(var i = 0, len = this.length; i < len; i++) {
+        if(this[i] && this[i].parentElement) {
+            this[i].parentElement.removeChild(this[i]);
+        }
+    }
+}
+
+function addCragInit(){
+    var add_but = document.querySelector("#newCragButton");
+    var cname = document.querySelector("#cragName");
+    var locat = document.querySelector("#location");
+    var tbody = document.querySelector("#cragsTableBody");
+    if (add_but == null) return;
+    
+    function clear_fields(){
+        cname.value = "";
+        locat.value = "";
+    }
+
+    add_but.addEventListener('click',function(evt){
+        var rec = {
+            cragName : cname.value,
+            location : locat.value
+        };
+        var json = JSON.stringify(rec);
+        var r = jsRoutes.controllers.Crags.addCrag();
+        local_ajax_mod.ajax_request({
+            method: "POST",
+            link: r.url,
+            mime: 'application/json',
+            doc: json,
+            ok_fn: function(req) {
+                try {
+                    var tr = document.createElement("tr");
+                    tr.setAttribute("id",cname.value);
+                    tr.innerHTML = "<td>" +
+                    "<a href=\"@routes.Boulders.filteredList(" + cname.value + ")\">" +
+                    "<h5>" + cname.value + "</h5></a></td><td><h5>" +
+                    locat.value + "</h5></td><td><button id=\"deleteCragButton\" class=\"btn btn-danger\" " +
+                    "onclick=\"deleteCrag('" + cname.value + "')\">Delete</button></td>";
+                    tbody.appendChild(tr);
+                    clear_fields();
+                }
+                catch(e){
+                    console.log(e);
+                }
+            },
+            err_fn: function(req) {
+                window.alert("failed: " + req);
+            }
+        }, false);
+    }); 
+}
+
+function deleteCrag(name){
+    var rec = {
+        cragName : name
+    };
+    var json = JSON.stringify(rec);
+    var r = jsRoutes.controllers.Crags.deleteCrag(name);
+    local_ajax_mod.ajax_request({
+        method: "DELETE",
+        link: r.url,
+        mime: 'application/json',
+        doc: json,
+        ok_fn: function(req){
+            var tr = document.getElementById(name).remove();
+        },
+        err_fn: function(req){
+            window.alert("Failed: " + req);
+        }
+    }, false);
 }
